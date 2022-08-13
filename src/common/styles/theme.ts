ecedf2971ffc407218de926b628005ac2bfe0768
Gabriel Santiago <gabrielmssantiago@gmail.com>
2022-08-13 11:30:34 -0300

    create theme palette

--- src/common/styles/theme.ts
+++ src/common/styles/theme.ts
@@ -3,6 +3,18 @@ const theme = {
     primary: {
       main: '#1D2527',
     },
+    secondary: {
+      main: '#AFAFAF',
+    },
+    tertiary: {
+      main: '#D2D4D9',
+    },
+    common: {
+      white: '#FFFFFF',
+    },
+    background: {
+      main: '#EBEFF2',
+    },
   },
   spacing: (factor: number) => `${0.5 * factor}rem`,
 };
