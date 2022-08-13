2b5ef1a95d0d54b8787d84422ff0258e316fda75
Gabriel Santiago <gabrielmssantiago@gmail.com>
2022-08-13 20:44:17 -0300

    button tests

---
+++ src/components/Button/Base/__tests__/BaseButton.test.tsx
@@ -0,0 +1,27 @@
+import { render, userEvent, screen } from 'test-utils';
+import Button from '../index';
+
+// em um design system real, testaria as variantes de estilos, mas por falta de tempo, neste desafio, dei preferencia a outras coisas mais importantes
+
+it('should call function on click', () => {
+  const func = jest.fn();
+  render(<Button onClick={func()}>click me</Button>);
+  userEvent.click(screen.getByRole('button', { name: 'click me' }));
+  expect(func).toHaveBeenCalled();
+});
+
+it('should display icon', () => {
+  const FakeIcon = () => <div>fake icon</div>;
+  render(<Button startIcon={<FakeIcon />}>click me</Button>);
+  expect(screen.getByText('fake icon')).toBeInTheDocument();
+});
+
+it('should render children', () => {
+  const FakeChildren = () => <div>fake children</div>;
+  render(
+    <Button>
+      <FakeChildren />
+    </Button>,
+  );
+  expect(screen.getByText('fake children')).toBeInTheDocument();
+});
