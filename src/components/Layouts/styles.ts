cfdfbeca0a47944afe9b0dec679e4e9b914609c3
Gabriel Santiago <gabrielmssantiago@gmail.com>
2022-08-14 02:52:22 -0300

    fix layout

--- src/components/Layouts/styles.ts
+++ src/components/Layouts/styles.ts
@@ -1,20 +1,16 @@
 import styled from 'styled-components';
 
 export const Main = styled.main`
-  width: '100%';
-  max-width: '1440px';
-  margin: '0 auto';
+  flex: 1;
   padding-left: ${({ theme }) => theme.spacing(2)};
   padding-right: ${({ theme }) => theme.spacing(2)};
-  flex: 1;
   margin-top: ${({ theme }) => theme.spacing(6)};
   margin-bottom: ${({ theme }) => theme.spacing(6)};
 `;
 
 export const Container = styled.div`
-  display: 'flex';
-  flex-direction: 'column';
-  height: '100%';
-  width: '100%';
-  flex: 1;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
 `;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,24 +5,6 @@ import Input from 'components/Input';
 const Home = () => (
   <section>
     <h1>Home page</h1>
-    <Input label="test" name="name" />
-    <Button>dewde</Button>
-    <CarInfoCard
-      id={1}
-      name="Mustang"
-      year="2015"
-      maxSpeed={180}
-      economyRate={9}
-      usersRate={8}
-    />
-    <CarInfoCard
-      id={2}
-      name="Mustang"
-      year="2015"
-      maxSpeed={180}
-      economyRate={9}
-      usersRate={8}
-    />
   </section>
 );
 
