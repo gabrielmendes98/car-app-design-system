4c68dea6655ebfa636705254ab79e7f56ba10a68
Gabriel Santiago <gabrielmssantiago@gmail.com>
2022-08-13 21:42:16 -0300

    create text tests

---
+++ src/components/Text/Base/__tests__/BaseText.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from 'test-utils';
+import Text from '../index';
+
+// em um design system real, testaria as variantes de estilos, mas por falta de tempo, neste desafio, dei preferencia a outras coisas mais importantes
+
+it('should render p element when p1 variant', () => {
+  render(<Text variant="p1">text</Text>);
+  expect(document.querySelector('p')).toBeInTheDocument();
+});
+
+it('should render p element when p2 variant', () => {
+  render(<Text variant="p2">text</Text>);
+  expect(document.querySelector('p')).toBeInTheDocument();
+});
+
+it('should render h1 element when h1 variant', () => {
+  render(<Text variant="h1">text</Text>);
+  expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
+});
+
+it('should render h2 element when h2 variant', () => {
+  render(<Text variant="h2">text</Text>);
+  expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+});
