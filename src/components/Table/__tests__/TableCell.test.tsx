b2183eaf2832bb122db6e87ff6ac4494699a8b46
Gabriel Santiago <gabrielmssantiago@gmail.com>
2022-08-17 10:54:10 -0300

    test table cell

---
+++ src/components/Table/__tests__/TableCell.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from 'test-utils';
+import TableCell from '../Cell';
+
+it('should render td element by default', () => {
+  render(
+    <table>
+      <tbody>
+        <tr>
+          <TableCell />
+        </tr>
+      </tbody>
+    </table>,
+  );
+  expect(document.querySelector('td')).toBeInTheDocument();
+});
+
+it('should be able to change element to th', () => {
+  render(
+    <table>
+      <thead>
+        <tr>
+          <TableCell as="th" />
+        </tr>
+      </thead>
+    </table>,
+  );
+  expect(document.querySelector('th')).toBeInTheDocument();
+});
